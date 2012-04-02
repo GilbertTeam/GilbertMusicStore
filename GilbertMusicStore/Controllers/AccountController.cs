750c86ccfddff671665536263620e87d77387021
Tenere <v.bessonov@hotmail.com>
2012-04-02 23:50:28 +0500

    Вернул старую модель аутентификации.

--- GilbertMusicStore/Controllers/AccountController.cs
+++ GilbertMusicStore/Controllers/AccountController.cs
@@ -13,196 +13,169 @@ namespace GilbertMusicStore.Controllers
 	{
 		#region Methods
 
-        //
-        // GET: /Account/Admin
-
-        public ActionResult Admin()
-        {
-                return View();
-        }
-
-		//
-		// GET: /Account/LogOn
-
 		#region Status Codes
 
 		private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-{
-// See http://go.microsoft.com/fwlink/?LinkID=177550 for
-// a full list of status codes.
-switch (createStatus)
-{
-case MembershipCreateStatus.DuplicateUserName:
-return "User name already exists. Please enter a different user name.";
+		{
+			// See http://go.microsoft.com/fwlink/?LinkID=177550 for
+			// a full list of status codes.
+			switch (createStatus)
+			{
+			case MembershipCreateStatus.DuplicateUserName:
+			return "User name already exists. Please enter a different user name.";
 
-case MembershipCreateStatus.DuplicateEmail:
-return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+			case MembershipCreateStatus.DuplicateEmail:
+			return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
 
-case MembershipCreateStatus.InvalidPassword:
-return "The password provided is invalid. Please enter a valid password value.";
+			case MembershipCreateStatus.InvalidPassword:
+			return "The password provided is invalid. Please enter a valid password value.";
 
-case MembershipCreateStatus.InvalidEmail:
-return "The e-mail address provided is invalid. Please check the value and try again.";
+			case MembershipCreateStatus.InvalidEmail:
+			return "The e-mail address provided is invalid. Please check the value and try again.";
 
-case MembershipCreateStatus.InvalidAnswer:
-return "The password retrieval answer provided is invalid. Please check the value and try again.";
+			case MembershipCreateStatus.InvalidAnswer:
+			return "The password retrieval answer provided is invalid. Please check the value and try again.";
 
-case MembershipCreateStatus.InvalidQuestion:
-return "The password retrieval question provided is invalid. Please check the value and try again.";
+			case MembershipCreateStatus.InvalidQuestion:
+			return "The password retrieval question provided is invalid. Please check the value and try again.";
 
-case MembershipCreateStatus.InvalidUserName:
-return "The user name provided is invalid. Please check the value and try again.";
+			case MembershipCreateStatus.InvalidUserName:
+			return "The user name provided is invalid. Please check the value and try again.";
 
-case MembershipCreateStatus.ProviderError:
-return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+			case MembershipCreateStatus.ProviderError:
+			return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
 
-case MembershipCreateStatus.UserRejected:
-return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+			case MembershipCreateStatus.UserRejected:
+			return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
 
-default:
-return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-}
-}
-#endregion
+			default:
+			return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+			}
+		}
+		#endregion
 
-private void CheckoutShoppingCart(string userName)
-{
-ShoppingCart shoppingCart = ShoppingCart.GetCart(HttpContext);
+		private void CheckoutShoppingCart(string userName)
+		{
+			ShoppingCart shoppingCart = ShoppingCart.GetCart(HttpContext);
 
-shoppingCart.Checkout(userName);
-Session[ShoppingCart.SessionKey] = userName;
-}
+			shoppingCart.Checkout(userName);
+			Session[ShoppingCart.SessionKey] = userName;
+		}
 
-public ActionResult LogOn()
-{
-return View();
-}
+		public ActionResult LogOn()
+		{
+			return View();
+		}
 
-[HttpPost]
-public ActionResult LogOn(LogOnModel model, string returnUrl)
-{
-if (ModelState.IsValid)
-{
-if (Membership.ValidateUser(model.UserName, model.Password))
-{
-CheckoutShoppingCart(model.UserName);
+		[HttpPost]
+		public ActionResult LogOn(LogOnModel model, string returnUrl)
+		{
+			if (ModelState.IsValid)
+			{
+				if (Membership.ValidateUser(model.UserName, model.Password))
+				{
+					CheckoutShoppingCart(model.UserName);
 
-FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-&& !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-{
-return Redirect(returnUrl);
-}
-else
-{
-						return RedirectToAction("Index", "Home");
-}
-}
-else
-{
-ModelState.AddModelError(string.Empty, Properties.Resources.AuthorizationError);
-}
-}
+					FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+					if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+						&& !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+					{
+						return Redirect(returnUrl);
+					}
+					else
+					{
+						return RedirectToAction("Index", "News");
+					}
+				}
+				else
+				{
+					ModelState.AddModelError(string.Empty, Properties.Resources.AuthorizationError);
+				}
+			}
 
-return View(model);
-}
+			return View(model);
+		}
 
-public ActionResult LogOff()
-{
-FormsAuthentication.SignOut();
+		public ActionResult LogOff()
+		{
+			FormsAuthentication.SignOut();
 
-			return RedirectToAction("Index", "Home");
-}
+			return RedirectToAction("Index", "News");
+		}
 
-public ActionResult Register()
-{
-return View();
-}
+		public ActionResult Register()
+		{
+			return View();
+		}
 
-//
-// POST: /Account/Register
+		[HttpPost]
+		public ActionResult Register(RegisterModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				MembershipCreateStatus createStatus;
+				Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
 
-[HttpPost]
-public ActionResult Register(RegisterModel model)
-{
-if (ModelState.IsValid)
-{
-MembershipCreateStatus createStatus;
-Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
+				if (createStatus == MembershipCreateStatus.Success)
+				{
+					CheckoutShoppingCart(model.UserName);
 
-if (createStatus == MembershipCreateStatus.Success)
-{
-CheckoutShoppingCart(model.UserName);
+					FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
+					return RedirectToAction("Index", "News");
+				}
+				else
+				{
+					ModelState.AddModelError("", ErrorCodeToString(createStatus));
+				}
+			}
 
-FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
-return RedirectToAction("Index", "Home");
-}
-else
-{
-ModelState.AddModelError("", ErrorCodeToString(createStatus));
-}
-}
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 
-// If we got this far, something failed, redisplay form
-return View(model);
-}
+		[Authorize]
+		public ActionResult ChangePassword()
+		{
+			return View();
+		}
 
-//
-// GET: /Account/ChangePassword
+		[Authorize]
+		[HttpPost]
+		public ActionResult ChangePassword(ChangePasswordModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				// ChangePassword will throw an exception rather
+				// than return false in certain failure scenarios.
+				bool changePasswordSucceeded;
+				try
+				{
+					MembershipUser currentUser = Membership.GetUser(User.Identity.Name, true /* userIsOnline */);
+					changePasswordSucceeded = currentUser.ChangePassword(model.OldPassword, model.NewPassword);
+				}
+				catch (Exception)
+				{
+					changePasswordSucceeded = false;
+				}
 
-[Authorize]
-public ActionResult ChangePassword()
-{
-return View();
-}
+				if (changePasswordSucceeded)
+				{
+					return RedirectToAction("ChangePasswordSuccess");
+				}
+				else
+				{
+					ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
+				}
+			}
 
-//
-// POST: /Account/ChangePassword
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 
-[Authorize]
-[HttpPost]
-public ActionResult ChangePassword(ChangePasswordModel model)
-{
-if (ModelState.IsValid)
-{
-
-// ChangePassword will throw an exception rather
-// than return false in certain failure scenarios.
-bool changePasswordSucceeded;
-try
-{
-MembershipUser currentUser = Membership.GetUser(User.Identity.Name, true /* userIsOnline */);
-changePasswordSucceeded = currentUser.ChangePassword(model.OldPassword, model.NewPassword);
-}
-catch (Exception)
-{
-changePasswordSucceeded = false;
-}
-
-if (changePasswordSucceeded)
-{
-return RedirectToAction("ChangePasswordSuccess");
-}
-else
-{
-ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
-}
-}
-
-// If we got this far, something failed, redisplay form
-return View(model);
-}
-
-//
-// GET: /Account/ChangePasswordSuccess
-
-public ActionResult ChangePasswordSuccess()
-{
-return View();
-}
-
-		
-		
-#endregion
-}
+		public ActionResult ChangePasswordSuccess()
+		{
+			return View();
+		}
+		#endregion
+	}
 }
--- GilbertMusicStore/Global.asax.cs
+++ GilbertMusicStore/Global.asax.cs
@@ -11,52 +11,21 @@ namespace GilbertMusicStore
 
 	public class MvcApplication : System.Web.HttpApplication
 	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		protected void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
 			filters.Add(new HandleErrorAttribute());
 		}
 
-		public static void RegisterRoutes(RouteCollection routes)
+		protected void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
-				"Default", // Route name
-				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Store", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+				"Default",
+				"{controller}/{action}/{id}",
+				new { controller = "News", action = "Index", id = UrlParameter.Optional }
 			);
-
 		}
-
-        public static void AddAdmin()
-        {
-            // Если нет в системе роли admin, создаём её
-            if (!Roles.RoleExists("admin"))
-                Roles.CreateRole("admin");
-
-            if (!Roles.RoleExists("Administrator"))
-                Roles.CreateRole("Administrator");
-
-            // Если нет в системе пользователя admin, создаём его
-            if (Membership.GetUser("admin") == null)
-            {
-                MembershipCreateStatus status = MembershipCreateStatus.Success;
-                Membership.CreateUser("admin", "temp_pass", "temp@temp.com", "2*2", "4", true, out status);
-            }
-
-            if (Membership.GetUser("Administrator") == null)
-            {
-                MembershipCreateStatus status = MembershipCreateStatus.Success;
-                Membership.CreateUser("Administrator", "Qwerty12345", "temp@temp.com", "2*2", "4", true, out status);
-            }
-
-            // Если у пользователя admin нет роли admin, присваиваем ему эту роль
-            if (!Roles.IsUserInRole("Administrator", "admin"))
-                Roles.AddUserToRole("Administrator", "admin");
-
-            if (!Roles.IsUserInRole("Administrator", "Administrator"))
-                Roles.AddUserToRole("Administrator", "Administrator");
-        }
 
 		protected void Application_Start()
 		{
@@ -65,9 +34,7 @@ namespace GilbertMusicStore
 			AreaRegistration.RegisterAllAreas();
 
 			RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
-
-            AddAdmin();
+			RegisterRoutes(RouteTable.Routes);
 		}
 	}
 }
